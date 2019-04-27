29f5dbad1f2d1d6d563760d5c85cf62b44ed6f2c
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 12:38:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var clone = Object.assign({}, object)
-  delete Object
+  delete clone[key]
   return clone
 }
