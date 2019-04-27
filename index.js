7d866107179c3587112ee9d102a574ee2d022ec4
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 12:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object.assign({}, object, {key:value});
+  return object.assign({}, object, {[key]: value});
 }
